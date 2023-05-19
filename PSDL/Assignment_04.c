b97799a2969536f56b77c646203cd8b7c8443749
OmNikam04 <80584774+OmNikam04@users.noreply.github.com>
2023-05-19 15:30:48 +0530

    Update Assignment_04.c

--- PSDL/Assignment_04.c
+++ PSDL/Assignment_04.c
@@ -1,8 +1,8 @@
 #include <pic18f4550.h>
 
 void delay(){
-    T0CON = 0x01;
-    TMR0 = 65316;
+    T0CON = 0x07;
+    TMR0 = 18720;
     T0CONbits.TMR0ON = 1;
     INTCONbits.TMR0IF = 0;
 
@@ -14,7 +14,7 @@ void delay(){
 
 void main() {
     TRISB = 0;
-    PORTB = 0x55;
+    PORTB = 0x55;// use binary values here to perform pattern blinking like 0b01010104;
     while(1){
         delay();
         PORTB = ~PORTB;
